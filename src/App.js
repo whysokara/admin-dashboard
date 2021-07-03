febd87355a8d5adba2694beb7d382a7b41c567d8
Himanshu Kara <kara_himanshu@yahoo.com>
2021-07-04 00:09:27 +0530

    User Edit Page (...)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./app.css";
 import Home from "./components/pages/home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserList from "./components/pages/userList/UserList";
+import User from "./components/pages/user/User";
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
           </Route>
           <Route path="/users">
             <UserList />
+          </Route>
+          <Route path="/user/:userId">
+            <User />
           </Route>
         </Switch>
       </div>
