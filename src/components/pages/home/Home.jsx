7aa97934c685fdd65dda81167792f0e9053aad9d
Himanshu Kara <kara_himanshu@yahoo.com>
2021-07-03 18:40:55 +0530

    Small Widget Added

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import Chart from "../../chart/Chart";
 import FeaturedInfo from "../../featuredInfo/FeaturedInfo";
 import "./home.css";
 import { userData } from "../../../dummyData";
+import WidgetLg from "../../widgetLg/WidgetLg";
+import WidgetSm from "../../widgetSm/WidgetSm";
 
 export default function Home() {
   return (
@@ -13,6 +15,10 @@ export default function Home() {
         dataKey="Active User"
         grid
       />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
     </div>
   );
 }
